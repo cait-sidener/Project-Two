74286648d751bea2f3994c2dfd7b60ffdeadf448
Cait Sidener <cait.sidener@gmail.com>
2019-03-15 22:15:05 -0500

    edits

---
+++ models/survey.js
@@ -0,0 +1,32 @@
+// Creating our Survey model
+module.exports = function(sequelize, DataTypes) {
+  var Survey = sequelize.define("Survey", {
+    // The email cannot be null, and must be a proper email before creation
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    // The password cannot be null
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    html: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    css: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    javascript: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+  return Survey;
+};
