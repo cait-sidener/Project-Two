3b760218c8c00caa9e28e74baac5e620c948bcab
beshoyr4 <33008284+beshoyr4@users.noreply.github.com>
2019-03-18 04:13:13 -0500

    added chatroom

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var session = require("express-session");
 
-// Requiring for video conference. NOTE: npm install --save socket.io
-var io = require("socket.io")();
+//chatroom
+var server = require('http').createServer(app);
+var path = require('path');
+var server = require('http').createServer(app);
+
+
+// Requiring for video conference/ and chatRoom. NOTE: npm install --save socket.io
+var io = require("socket.io")(server);
 
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
@@ -13,6 +19,79 @@ var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// Routing for chatroom
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
