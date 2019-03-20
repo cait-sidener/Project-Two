4a7f93b8c24ab69e3a5d77252d28580e8ad6ae88
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-03-19 21:01:14 -0500

    works i think

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Project-Two
+# **Study Buddy**
+
+## **Overview**
+Study Buddy is an application that connects people who want to expand their knowledge of specific topics. 
+
+This application connects people with similar interests, matched by level of skill, and promotes collaboration in learning between individuals regardless of geographic location.
+
+**Deploy App**
+
+Visit this site to deploy the app!
+
+(insert Heroku link)
+
+## **Technical Details**
+**Technologies Used**
+
+Study Buddy uses the following technologies:
+- Node
+- Express
+- Handlebars
+- Sequelize
+- Passport
+- Socket.io
+- PubNub
+- Restful APIs
+
+**NPM Installations**
+
+Navigate to the root of your project. Then in the terminal command line run npm init. This will initalize a package.json for your project.
+
+The following NPM installations are included:
+- npm install chat-engine
+- npm install chat-engine-webrtc
+- npm install express
+- npm install express-handlebars
+- npm install mysql
+- npm install passport
+- npm install --save path
+- npm install --save sequelize
+- npm install socket.io
+
+**Explanation**
+
+The application uses...
+
+## **Screenshots**
+
+The home page will...
+
+![Home](LINK)
+
+
+The user will...
+
+![Survey](LINK)
+
+**Thank you for visiting my FriendFinder project!**
+
+## **Author**
+
+**Ashley Shaw** - https://ashbshaw.github.io/Ashley-Shaw-Portfolio/
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,11 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 // Use CRUD function to add, read, update, and delete class options
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
@@ -14,7 +14,7 @@ module.exports = function(app) {
       where: {
         email: req.body.email
       }
-    }).then(function(result) {
+    }).then(function (result) {
       res.json("/members/" + result.id);
     });
   });
@@ -22,16 +22,16 @@ module.exports = function(app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     console.log(req.body);
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
         res.json(err);
         // res.status(422).json(err.errors[0].message);
@@ -39,13 +39,13 @@ module.exports = function(app) {
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -60,7 +60,7 @@ module.exports = function(app) {
   });
   // Survey Post
   // POST route for saving a new survey
-  app.post("/api/survey", function(req, res) {
+  app.post("/api/survey", function (req, res) {
     // create takes an argument of an object describing the item we want to insert
     // into our table. In this case we just we pass in an object with a text and
     // complete property
@@ -80,23 +80,24 @@ module.exports = function(app) {
         css: userData.css,
         javascript: userData.javascript
       }
-    }).then(function(result) {
+    }).then(function (result) {
       bestMatch = result[0];
       console.log(result);
-      db.Survey.create(userData).then(function() {
+      db.Survey.create(userData).then(function () {
         // res.json(result);
       });
     });
   });
 
   // FindOne
-  app.get("/api/survey/:id", function(req, res) {
+  app.get("/api/survey/:id", function (req, res) {
     db.Survey.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function(dbSurvey) {
+    }).then(function (dbSurvey) {
       res.json(dbSurvey);
     });
   });
-};
+
+  };
